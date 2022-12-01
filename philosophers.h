8f44b538b536c45a76d031c81c897b7e6634752a
molhot <sakata19991214@gmail.com>
2022-12-01 23:20:09 +0900

    philophilo

---
+++ philosophers.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 22:57:45 by satushi           #+#    #+#             */
+/*   Updated: 2022/12/01 23:03:22 by satushi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERES_H
+# define PHILOSOPHERES_H
+
+#ifndef number_of_philosophers
+# define number_of_philosophers
+#endif
+
+#ifndef time_to_die
+# define time_to_die
+#endif
+
+#ifndef time_to_eat
+# define time_to_eat
+#endif
+
+#ifndef time_to_sleep
+# define time_to_sleep
+#endif
+
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stddef.h>
+
+#endif
